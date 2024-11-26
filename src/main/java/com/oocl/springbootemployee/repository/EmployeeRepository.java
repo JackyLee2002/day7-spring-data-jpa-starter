52cb47cda2084c9c408b97198a70e17ac6ea6ccf
Jacky Lee <leeja12@oocl.com>
2024-11-26 15:46:03 +0800

    test: successfully pass test should_throw_EmployeeAgeNotValidException_when_create_given_a_employee_with_age_17

--- src/main/java/com/oocl/springbootemployee/repository/EmployeeRepository.java
+++ src/main/java/com/oocl/springbootemployee/repository/EmployeeRepository.java
@@ -2,9 +2,12 @@ package com.oocl.springbootemployee.repository;
 
 import com.oocl.springbootemployee.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
+    @Query
+    Employee findEmployeeById(Integer Id);
 }
--- src/test/java/com/oocl/springbootemployee/service/EmployeeServiceTest.java
+++ src/test/java/com/oocl/springbootemployee/service/EmployeeServiceTest.java
@@ -63,13 +63,11 @@ class EmployeeServiceTest {
     @Test
     void should_throw_EmployeeAgeNotValidException_when_create_given_a_employee_with_age_17() {
         //given
-        EmployeeInMemoryRepository mockedEmployeeInMemoryRepository = mock(EmployeeInMemoryRepository.class);
         Employee kitty = new Employee(1, "Kitty", 6, Gender.FEMALE, 8000.0);
-        EmployeeService employeeService = new EmployeeService(mockedEmployeeInMemoryRepository,employeeRepository);
         //when
         //then
         assertThrows(EmployeeAgeNotValidException.class, () -> employeeService.create(kitty));
-        verify(mockedEmployeeInMemoryRepository, never()).create(any());
+        verify(employeeRepository, never()).save(any());
     }
 
     @Test
