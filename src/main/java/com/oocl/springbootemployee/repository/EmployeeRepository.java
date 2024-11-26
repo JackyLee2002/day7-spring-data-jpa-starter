955479a63f6947b15860ccbba9718eec02a70cc6
Jacky Lee <leeja12@oocl.com>
2024-11-26 16:03:13 +0800

    test: findEmployeesByGender jpa query

--- src/main/java/com/oocl/springbootemployee/repository/EmployeeRepository.java
+++ src/main/java/com/oocl/springbootemployee/repository/EmployeeRepository.java
@@ -1,13 +1,17 @@
 package com.oocl.springbootemployee.repository;
 
 import com.oocl.springbootemployee.model.Employee;
+import com.oocl.springbootemployee.model.Gender;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
-    @Query
-    Employee findEmployeeById(Integer Id);
+    List<Employee> findEmployeesByGender(Gender gender);
+
+    
+
 }
